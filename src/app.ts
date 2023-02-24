675cee4aa5f854c05af9a5b5dace66d8fe03bc70
Robbna <robb.navhrod@gmail.com>
2023-02-24 19:46:43 +0100

    Characters endpoint refactor

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { PotionsEndpoint } from './model/endpoint/PotionsEndpoint';
 import { SpellsEndpoint } from './model/endpoint/SpellsEndpoint';
-import { CharacterEndpoint } from './model/endpoint/CharacterEndpoint';
+import { CharactersEndpoint } from './model/endpoint/CharactersEndpoint';
 import express from "express";
 import dotenv from "dotenv";
 import { BooksEndpoint } from './model/endpoint/BooksEndpoint';
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 
-new CharacterEndpoint(app, "/api/characters").initialize();
+new CharactersEndpoint(app, "/api/characters").initialize();
 new SpellsEndpoint(app, "/api/spells").initialize();
 new PotionsEndpoint(app, "/api/potions").initialize();
 new BooksEndpoint(app, "/api/books").initialize();
--- src/model/endpoint/CharactersEndpoint.ts
+++ src/model/endpoint/CharactersEndpoint.ts
@@ -1,7 +1,7 @@
 import { IEndpoint } from "../interface/IEndpoint";
 import { Endpoint } from "../super/Endpoint";
 import data from "../../data/characters.json";
-export class CharacterEndpoint extends Endpoint implements IEndpoint {
+export class CharactersEndpoint extends Endpoint implements IEndpoint {
   initialize(): void {
     this.app.get(this.url, (req, res) => {
       res.json(data);
