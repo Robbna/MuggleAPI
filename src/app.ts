c94210eb143bc99d25ce9d1da2027a1770197e06
Robert <robb.navhrod@gmail.com>
2023-03-08 13:54:02 +0100

    refactored tabs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import { PotionsEndpoint } from './model/endpoints/PotionsEndpoint';
-import { SpellsEndpoint } from './model/endpoints/SpellsEndpoint';
+import { PotionsEndpoint }    from './model/endpoints/PotionsEndpoint';
+import { SpellsEndpoint }     from './model/endpoints/SpellsEndpoint';
 import { CharactersEndpoint } from './model/endpoints/CharactersEndpoint';
-import express from "express";
-import dotenv from "dotenv";
-import { BooksEndpoint } from './model/endpoints/BooksEndpoint';
+import { BooksEndpoint }      from './model/endpoints/BooksEndpoint';
+import express  from "express";
+import dotenv   from "dotenv";
 dotenv.config();
 
 const app = express();
@@ -12,6 +12,7 @@ app.use("/api", (_req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
+
 new CharactersEndpoint(app, "/api/characters").initialize();
 new SpellsEndpoint(app, "/api/spells").initialize();
 new PotionsEndpoint(app, "/api/potions").initialize();
